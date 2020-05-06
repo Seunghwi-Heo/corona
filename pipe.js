b8d24fb352c846d630012833cf4c74b945ba3dd7
jhy <v2red@naver.com>
2020-05-06 17:39:37 +0900

    데이터 지우기

--- pipe.js
+++ pipe.js
@@ -1,9 +1,11 @@
 var data = require('./data.js')
 var redisinserter = require('./redisinserter.js')
+var redisdelete = require('./redisdelete.js')
 var schedule = require('node-schedule')
 
 
 async function pipe(){
+    await redisdelete.redisdelete()
     await data.data()
     await redisinserter.redisinserter()
 
